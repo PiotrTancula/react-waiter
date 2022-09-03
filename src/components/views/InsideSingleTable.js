cac6429b9744f422e49a739d32f8e809583f17fd
Piotr Tańcula <tanculapiotr@gmail.com>
2022-09-03 10:47:39 +0200

    InsideSingleTable form issues,cannot update people,maxpeople

--- src/components/views/InsideSingleTable.js
+++ src/components/views/InsideSingleTable.js
@@ -2,16 +2,27 @@ import { useParams } from "react-router-dom";
 import { Form,Container,Row,Col,Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { getTableById } from "../../redux/tablesRedux";
+import { editTable, getTableById } from "../../redux/tablesRedux";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { editTableInfo } from "../../redux/tablesRedux";
+import { useNavigate } from "react-router-dom";
 
 const InsideSingleTable = () => {
 
+  const { register, handleSubmit: validate, formState: { errors } } = useForm();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { id } = useParams();
   const table = useSelector(state => getTableById(state, id));
-  const [statuses, setStatuses] = useState([]);
 
+  const [tableId, setTableId] = useState(0);
+  const [statuses, setStatuses] = useState([]);
   const [people, setPeople] = useState(0);
   const [maxPeople, setMaxPeople] = useState(0);
+  const [bill, setBill] = useState(0);
+  const [status, setStatus] = useState('');
 
   // on startup
   useEffect(() => {
@@ -21,21 +32,29 @@ const InsideSingleTable = () => {
   }, []);
 
   useEffect(() => {
-    if(table) {
+    if (table) {
+      setTableId(table.id);
+      setStatus(table.status);
       setPeople(table.peopleAmount);
       setMaxPeople(table.maxPeopleAmount);
+      setBill(table.bill);
     }
   }, [table])
+
+  const handleSubmit = () => {
+    dispatch(editTableInfo({ id, status, people, maxPeople, bill }));
+    navigate('/');
+  }
 
   if (!table) return <h2>Loading...</h2>
   return (
     <>
       <h1>Table {id} </h1>
 
-      <Form>
+      <Form onSubmit={validate(handleSubmit)} className="w-100">
         <Form.Group className="d-flex flex-row align-items-center">
           <Form.Label className="px-3 py-3">Status: </Form.Label>
-          <Form.Select className="w-25" aria-label="Default select example">
+          <Form.Select className="w-25" aria-label="Default select example" onChange={e => setStatus(e.target.value)} value={status}>
             <option>Open this select menu</option>
             {statuses.map(status => <option key={status.id} >{status.description}</option>)}
           </Form.Select>
@@ -43,25 +62,28 @@ const InsideSingleTable = () => {
 
         <Row>
           <Col md={3}>
-          <Form.Group className="d-flex flex-row align-items-center">
-            <Form.Label className="px-3 py-3">People: </Form.Label>
-              <Form.Control type="input" className="" value={people} />
-              <p className="px-3"> / </p>
-              <Form.Control type="input" className="" value={maxPeople} />
+          <Form.Group className="d-flex flex-row align-items-center w-100">
+              <Form.Label className="px-3 py-3">People: </Form.Label>
+                <Form.Control {...register('people', { required: true, min: 0, max: `${table.maxPeopleAmount}` })} type="input" className="" value={people} onChange={e => setPeople(e.target.value)} />
+                <p className="px-3"> / </p>
+              <Form.Control  type="input" className="" value={maxPeople} onChange = { e => setMaxPeople(e.target.value)} />
             </Form.Group>
           </Col>
         </Row>
-
+        {errors.people && <small className="w50 form-text text-danger mt-2">People's amount has to be between 0 and 10 but not bigger than the table's capacity</small>}
         <Row>
           <Col md={2}>
           <Form.Group className="d-flex flex-row align-items-center">
             <Form.Label className="px-3 py-3">Bill: </Form.Label>
             <p className="px-3"> $ </p>
-            <Form.Control type="input" className="" />
+            <Form.Control {...register('bill', { required: true, min: 1 }) } type="input" className="" value={bill} onChange={e => setBill(e.target.value)} />
+
           </Form.Group>
           </Col>
         </Row>
-
+        {errors.bill && <small className="w50 form-text text-danger mt-2">The bill has to have a value aboce 0 </small>}
+        <Row>
+        </Row>
         <Button type="submit" variant="primary" >update</Button>
 
         </Form>
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -4,11 +4,15 @@ export const getTableById = (state, id) => state.tables.find(table => table.id =
 
 // actions
 const createActionName = actionName => `app/tables/${actionName}`;
-const UPDATE_TABLES = createActionName('UPDATE_TABLES');
+const UPDATE_TABLES = createActionName('UPDATE_TABLES'); // homepage tables update
+const EDIT_TABLE = createActionName('EDIT_TABLE');
 
 // action creators
 
 export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
+export const editTable = payload => ({ type: EDIT_TABLE, payload });
+
+
 export const fetchTables = dispatch => {
   return (dispatch) => {
     fetch('http://localhost:3131/api/tables')
@@ -18,12 +22,28 @@ export const fetchTables = dispatch => {
   }
 };
 
+export const editTableInfo = (editedTable) => {
+  return (dispatch) => {
+    const options = {
+      method: 'PUT',
+      headers: {
+            'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(editedTable),
+    };
+    fetch(`http://localhost:3131/tables/${editedTable.id}`, options)
+      .then (()=> dispatch(editTable(editedTable)))
+  }
+}
+
 
 
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
     case UPDATE_TABLES:
       return [...action.payload]
+      case EDIT_TABLE:
+      return statePart.map((table) => (table.id === action.payload.id ? { ...table, ...action.payload } : table));
     default:
       return statePart;
   };
